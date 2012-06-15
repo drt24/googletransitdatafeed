aa62ab672bbe59f5b5201b564ef5c185b984c6eb
Daniel Thomas <drt24@cam.ac.uk>
2012-06-15 11:01:58 +0100

    Enable assertions when validating
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- java/Transxchange2GoogleTransit/validate.sh
+++ java/Transxchange2GoogleTransit/validate.sh
@@ -1,12 +1,12 @@
-java -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/linear.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val1
+java -ea -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/linear.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val1
 diff --strip-trailing-cr -q ValidatorFiles/val1 ValidatorFiles/ref1/20040922
-java -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/large.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val2
+java -ea -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/large.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val2
 diff --strip-trailing-cr -q ValidatorFiles/val2 ValidatorFiles/ref2/20040922
-java -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/eye.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val3
+java -ea -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/eye.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val3
 diff --strip-trailing-cr -q ValidatorFiles/val3 ValidatorFiles/ref3/20040301
-java -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/express.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val4
+java -ea -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/express.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val4
 diff --strip-trailing-cr -q ValidatorFiles/val4 ValidatorFiles/ref4/20040101
-java -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/cloverleaf.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val5
+java -ea -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/cloverleaf.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val5
 diff --strip-trailing-cr -q ValidatorFiles/val5 ValidatorFiles/ref5/20040101
-java -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/circular.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val6
+java -ea -jar dist/transxchange2GoogleTransit.jar ValidatorFiles/circular.xml http://www.bagency.org Europe/London 3 ValidatorFiles/val6
 diff --strip-trailing-cr -q ValidatorFiles/val6 ValidatorFiles/ref6/20040101
